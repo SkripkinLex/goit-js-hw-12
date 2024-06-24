f54d1caab7638eb717581ae37ae3227413df7789
Oleksii Bespalyi <skripkinlexxx@gmail.com>
2024-06-24 17:25:30 +0200

    add file

--- src/main.js
+++ src/main.js
@@ -132,7 +132,7 @@ loadMoreBtnEl.addEventListener('click', async e => {
   // Кількість груп в колекції
   const totalPages = Math.ceil(data.totalHits / perPage);
 
-  if (currentPage >= totalPages) {
+  if (pageNumber >= totalPages) {
     console.log('No more pages to load.');
     loadMoreBtnEl.disabled = true;
   } else {

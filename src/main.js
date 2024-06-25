d83968b4f4fc5b26eafa98f038b93339aca89fdf
Oleksii Bespalyi <skripkinlexxx@gmail.com>
2024-06-25 21:14:33 +0200

    add file

--- src/main.js
+++ src/main.js
@@ -1,13 +1,156 @@
+// import { renderCards, addLoader, removeLoader } from './js/render-functions.js';
+
+// import { sendQuery } from './js/pixabay-api.js';
+
+// import iziToast from 'izitoast';
+// import 'izitoast/dist/css/iziToast.min.css';
+
+// import SimpleLightbox from 'simplelightbox';
+// import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// import iconClose from './img/icon-close.svg';
+
+// // ---------------------------------------------------------
+// const formEl = document.querySelector('.search-form');
+// const inputEl = document.querySelector('.search-form__input');
+// const ulEl = document.querySelector('.gallery');
+// const loader = document.querySelector('.loader');
+// const loadMoreBtnEl = document.querySelector('.load-more__button');
+
+// export const perPage = 15;
+// export let pageNumber = 1;
+
+// let data = null;
+// let query;
+// let gallery;
+
+// gallery = new SimpleLightbox('.gallery a');
+
+// // ---------------------------------------------------------
+
+// function clearGallery() {
+//   ulEl.innerHTML = '';
+// }
+
+// function increasePage() {
+//   return (pageNumber = pageNumber + 1);
+// }
+
+// function hideLoadMoreBtn() {
+//   loadMoreBtnEl.classList.remove('active');
+// }
+
+// function resetPageNumber() {
+//   return (pageNumber = 1);
+// }
+
+// function checkEndPages(totalPages) {
+//   if (pageNumber > totalPages) {
+//     hideLoadMoreBtn();
+//     return iziToast.error({
+//       class: 'izt-toast-message',
+//       message: "We're sorry, but you've reached the end of search results.",
+//       messageSize: '16',
+//       messageLineHeight: '24',
+//       messageColor: '#ffffff',
+
+//       backgroundColor: '#b51b1b',
+//       iconUrl: iconClose,
+//       position: 'topRight',
+//       theme: 'dark',
+//     });
+//   }
+// }
+
+// async function loadMoreControle() {
+//   const totalPages = Math.ceil(data.totalHits / perPage);
+
+//   if (pageNumber >= totalPages) {
+//     console.log('No more pages to load.');
+//     loadMoreBtnEl.disabled = true;
+//     loadMoreBtnEl.style.display = "none";
+//   } else {
+//     data = await sendQuery(query);
+
+//     ulEl.insertAdjacentHTML('beforeend', renderCards(data.hits));
+//     gallery.refresh();
+
+//     increasePage();
+//   }
+// };
+
+// // ---------- Submit actions-----------------------------------------------
+
+// formEl.addEventListener('submit', async event => {
+//   event.preventDefault();
+
+//   const valueOfInput = event.target.elements.query.value.trim();
+//   query = valueOfInput;
+
+//   resetPageNumber();
+//   hideLoadMoreBtn();
+
+//   if (query.length !== 0) {
+//     addLoader(loader);
+
+//     try {
+//       data = await sendQuery(query); // {total: 24170, totalHits: 500, hits: Array(3)}
+//       loadMoreControle();
+//       // ---------------- If the data.hits === [] ---------
+
+//       if (data.hits.length === 0) {
+//         // ----- Using the library iziToast for message
+//         iziToast.show({
+//           class: 'izt-toast-message',
+
+//           message:
+//             'Sorry, there are no images matching your search query. Please try again!',
+//           messageSize: '16',
+//           messageLineHeight: '24',
+//           messageColor: '#ffffff',
+
+//           backgroundColor: '#b51b1b',
+//           iconUrl: iconClose,
+//           position: 'topRight',
+//           theme: 'dark',
+//         });
+
+//         // ----- Clear the gallery
+//         clearGallery();
+
+//         // ------------- If the data.hits not empty
+//       } else {
+//         clearGallery();
+
+//         ulEl.insertAdjacentHTML('beforeend', renderCards(data.hits));
+//         increasePage();
+
+
+//         gallery.refresh();
+
+//         loadMoreBtnEl.classList.add('active');
+//       }
+//       // -----------------------------------------
+//     } catch (err) {
+//       console.log(err);
+//     }
+
+//     removeLoader(loader);
+//   }
+
+//   formEl.reset();
+// });
+// // ------------------------------------------------
+// // -------------------- Click button's actions
+// loadMoreBtnEl.addEventListener('click', loadMoreControle);
+
+
 import { renderCards, addLoader, removeLoader } from './js/render-functions.js';
-
 import { sendQuery } from './js/pixabay-api.js';
-
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
-
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-
 import iconClose from './img/icon-close.svg';
 
 // ---------------------------------------------------------
@@ -70,14 +213,15 @@ async function loadMoreControle() {
     loadMoreBtnEl.disabled = true;
     loadMoreBtnEl.style.display = "none";
   } else {
-    data = await sendQuery(query);
+    data = await sendQuery(query, pageNumber, perPage);
 
     ulEl.insertAdjacentHTML('beforeend', renderCards(data.hits));
     gallery.refresh();
 
     increasePage();
+    checkEndPages(totalPages);
   }
-};
+}
 
 // ---------- Submit actions-----------------------------------------------
 
@@ -94,43 +238,33 @@ formEl.addEventListener('submit', async event => {
     addLoader(loader);
 
     try {
-      data = await sendQuery(query); // {total: 24170, totalHits: 500, hits: Array(3)}
+      data = await sendQuery(query, pageNumber, perPage); // {total: 24170, totalHits: 500, hits: Array(3)}
       loadMoreControle();
-      // ---------------- If the data.hits === [] ---------
 
       if (data.hits.length === 0) {
-        // ----- Using the library iziToast for message
         iziToast.show({
           class: 'izt-toast-message',
-
-          message:
-            'Sorry, there are no images matching your search query. Please try again!',
+          message: 'Sorry, there are no images matching your search query. Please try again!',
           messageSize: '16',
           messageLineHeight: '24',
           messageColor: '#ffffff',
-
           backgroundColor: '#b51b1b',
           iconUrl: iconClose,
           position: 'topRight',
           theme: 'dark',
         });
 
-        // ----- Clear the gallery
         clearGallery();
-
-        // ------------- If the data.hits not empty
       } else {
         clearGallery();
 
         ulEl.insertAdjacentHTML('beforeend', renderCards(data.hits));
         increasePage();
 
-
         gallery.refresh();
 
         loadMoreBtnEl.classList.add('active');
       }
-      // -----------------------------------------
     } catch (err) {
       console.log(err);
     }
